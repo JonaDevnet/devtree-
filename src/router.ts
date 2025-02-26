e27e2afb984b30c72d190d494d95750b89956828
JonaDevnet <jonathan.rinaldi03@gmail.com>
2025-02-26 12:30:07 -0300

    terminamos las demas validaciones respecto al usuario

--- src/router.ts
+++ src/router.ts
@@ -8,8 +8,22 @@ const router = Router();
 /**
  * Autentificacion y registro
  */
-router.post('/auth/register', 
-    body('handle').notEmpty(),
+router.post('/auth/register',
+
+    //reglas de validacion
+    body('handle')
+        .notEmpty()
+        .withMessage('El handle no puede ir vacio'),
+    body('name')
+        .notEmpty()
+        .withMessage('El name no puede ir vacio'),
+    body('email')
+        .isEmail()
+        .withMessage('El email no valido'),    
+    body('password')
+        .isEmail()
+        .withMessage('El password o puede ir vacio'),  
+
     createAccount )
 
 export default router;
