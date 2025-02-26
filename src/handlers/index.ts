e8ffcab51fd6ce6c735bea54a24edb1399061e3a
JonaDevnet <jonathan.rinaldi03@gmail.com>
2025-02-26 00:09:42 -0300

    agregando handle unico (usuario) y validacion para que no se repita

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,24 +1,42 @@
 import type { Request, Response } from "express";
+import slug from 'slug';
 import User from "../models/User";
 import { hashPassword } from "../utils/auth";
 
 export const createAccount = async (req: Request, res: Response) => {
 
     try {
-        const { email, password } = req.body;
+        //capturamos el cuerpo de la solicitud
+        const { email, password} = req.body;
 
+        // buscamos si ya existe
         const userExist = await User.findOne({email});
-    
+        
         if(userExist) {
-            const error = new Error('El error ya esta registrado');
+            const error = new Error('El correo ya esta registrado');
             res.status(409).json({error : error.message}); // recuperamos el mensaje de error
             return;
         }
         
+        // capturamos de la solicitud el handle
+        const handle = slug(req.body.handle, '');
+        // buscamos si ya existe
+        const userHandle = await User.findOne({email});
+
+        if(userHandle) {
+            const error = new Error('El usuario ya esta registrado');
+            res.status(409).json({error : error.message}); // recuperamos el mensaje de error
+            return;
+        }
+        // creamos el usuario con la solicitud
         const user = new User(req.body);
+
+        //hasheamos el pass del usuario y formateamos el handle
         user.password = await hashPassword(password);
+        user.handle = handle;
 
 
+        //guardamos
         await user.save();
 
         res.status(201).send({msg : 'Registro agregado'});
